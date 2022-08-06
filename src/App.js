d94a2ce16c5e4929d2f829bb5a747bd71b376423
akhiluv96 <91949933+akhiluv96@users.noreply.github.com>
2022-08-06 18:19:57 +0530

    Added filters

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import "antd/dist/antd.css";
 import axios from "axios";
+import Select from "react-select";
 import InputComp from "./components/InputComp";
 import TableComp from "./components/TableComp";
-import { Typography } from "antd";
-const { Text, Link } = Typography;
+import { Tag } from "antd";
 
 const url = "https://62e6bd340e5d74566aebd18b.mockapi.io/api/v1/products";
 
@@ -31,9 +31,13 @@ const columns = [
     key: "availability",
     render: (text) =>
       text === "in stock" ? (
-        <Text type="success">{text[0].toUpperCase() + text.slice(1)}</Text>
+        <Tag color={"green"} key={text}>
+          {text[0].toUpperCase() + text.slice(1)}
+        </Tag>
       ) : (
-        <Text type="danger">{text[0].toUpperCase() + text.slice(1)}</Text>
+        <Tag color={"red"} key={text}>
+          {text[0].toUpperCase() + text.slice(1)}
+        </Tag>
       ),
   },
 ];
@@ -42,14 +46,25 @@ function App() {
   const [search, setSearch] = useState("");
   const [products, setProducts] = useState([]);
   const [filterData, setFilterData] = useState([]);
-  const [error, setError] = useState("");
+  const [colorOption, setColorOption] = useState([]);
+  const [availOption, setAvailOption] = useState([]);
+  const [selectedColors, setSelectedColor] = useState([]);
+  const [selectedStatus, setSelectedStatus] = useState([]);
 
   useEffect(() => {
     axios
       .get(url)
       .then((res) => {
         const data = res.data.products.map((el) => ({ ...el, key: el.id }));
+        let colors = data.map((el) => el.color);
+        colors = [...new Set(colors)];
+        colors = colors.map((el) => ({ value: el, label: el }));
+        let avail = data.map((el) => el.availability);
+        avail = [...new Set(avail)];
+        avail = avail.map((el) => ({ value: el, label: el }));
         setProducts(data);
+        setColorOption(colors);
+        setAvailOption(avail);
       })
       .then((err) => {
         if (err) console.log(err);
@@ -66,6 +81,22 @@ function App() {
     }
   };
 
+  const handleColorFilter = (colors) => {
+    setSelectedColor(colors);
+    const data = search.length > 3 ? filterData : products;
+    const col = colors.map((el) => el.value);
+    const arr = data.filter((el) => col.includes(el.color));
+    setFilterData(arr);
+  };
+
+  const handleStatusFilter = (status) => {
+    setSelectedStatus(status);
+    const data = search.length > 3 ? filterData : products;
+    const sta = status.map((el) => el.value);
+    const arr = data.filter((el) => sta.includes(el.availability));
+    setFilterData(arr);
+  };
+
   return (
     <section className="App">
       <div className="header">
@@ -74,11 +105,35 @@ function App() {
           value={search}
           handleSearch={handleSearch}
         />
+        <div className="filters">
+          <div>
+            <Select
+              name="color"
+              isMulti
+              options={colorOption}
+              onChange={(color) => handleColorFilter(color)}
+            />
+          </div>
+          <div>
+            <Select
+              name="availability"
+              isMulti
+              options={availOption}
+              onChange={(status) => handleStatusFilter(status)}
+            />
+          </div>
+        </div>
       </div>
       <div>
         <TableComp
           columns={columns}
-          data={search.length > 3 && filterData ? filterData : products}
+          data={
+            selectedStatus.length > 0 ||
+            selectedColors.length > 0 ||
+            (search.length > 3 && filterData)
+              ? filterData
+              : products
+          }
         />
       </div>
     </section>
--- src/components/TableComp/index.js
+++ src/components/TableComp/index.js
@@ -1,12 +1,12 @@
-import React, {useEffect} from 'react';
-import { Table } from 'antd';
+import React from "react";
+import { Table } from "antd";
 
-const TableComp = ({columns, data}) => {
-    return (
-        <>
-            <Table columns={columns} dataSource={data} />
-        </>
-    )
-}
+const TableComp = ({ columns, data }) => {
+  return (
+    <>
+      <Table columns={columns} dataSource={data} />
+    </>
+  );
+};
 
 export default TableComp;
